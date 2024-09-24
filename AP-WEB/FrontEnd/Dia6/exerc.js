673954fb406d642dac9235445adb088cfbbf308f
Márcio Calisto <103325008+ClxtMf@users.noreply.github.com>
2024-09-24 20:35:36 -0300

    commit

---
+++ AP-WEB/FrontEnd/Dia6/exerc.js
@@ -0,0 +1,15 @@
+let comprimento_1 = 12;
+
+let comprimento_2 = 12;
+
+let comprimento_3 = 12;
+
+if (comprimento_1 != comprimento_2 || comprimento_2 != comprimento_3 || comprimento_1 != comprimento_3) {
+    console.log("O triângulo é Isósceles")
+} else if (comprimento_1 == comprimento_2 && comprimento_2 == comprimento_3) {
+    console.log("É um triângulo Equilátero")
+} else {
+    console.log("O triângulo é Escaleno")
+}
+
+
